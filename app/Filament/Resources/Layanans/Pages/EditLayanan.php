227b1440e39bdfee1da2d7c9db8367b21795b9aa
ahmadsepta2405 <ahmadsepta2405@gmail.com>
2026-04-21 22:03:20 +0700

    backend/katalog-layanan

---
+++ app/Filament/Resources/Layanans/Pages/EditLayanan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Layanans\Pages;
+
+use App\Filament\Resources\Layanans\LayananResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditLayanan extends EditRecord
+{
+    protected static string $resource = LayananResource::class;
+}
