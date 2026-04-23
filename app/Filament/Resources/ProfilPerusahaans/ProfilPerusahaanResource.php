2533725f126ca707c660bd24c2e56a03c6a6064e
ahmadsepta2405 <ahmadsepta2405@gmail.com>
2026-04-23 11:37:56 +0700

    feat: update bagian admin

--- app/Filament/Resources/ProfilPerusahaans/ProfilPerusahaanResource.php
+++ app/Filament/Resources/ProfilPerusahaans/ProfilPerusahaanResource.php
@@ -8,7 +8,6 @@ use App\Filament\Resources\ProfilPerusahaans\Pages\ListProfilPerusahaans;
 use App\Filament\Resources\ProfilPerusahaans\Schemas\ProfilPerusahaanForm;
 use App\Filament\Resources\ProfilPerusahaans\Tables\ProfilPerusahaansTable;
 use App\Models\ProfilPerusahaan;
-use BackedEnum;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
@@ -18,12 +17,19 @@ class ProfilPerusahaanResource extends Resource
 {
     protected static ?string $model = ProfilPerusahaan::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|null|\BackedEnum $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $label = 'Profil Perusahaan';
+    protected static ?string $pluralLabel = 'Profil Perusahaan';
+    protected static ?string $navigationLabel = 'Profil Perusahaan';
 
     protected static ?string $recordTitleAttribute = 'ProfilPerusahaan';
 
+    // TAMBAHKAN BARIS INI (Angka 2 biar di bawah Dashboard)
+    protected static ?int $navigationSort = 2;
+
     public static function form(Schema $schema): Schema
-    {
+    { 
         return ProfilPerusahaanForm::configure($schema);
     }
 
