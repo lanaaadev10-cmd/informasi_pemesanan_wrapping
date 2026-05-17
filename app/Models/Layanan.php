f617c06c30e3213c8204cb657eda5d6e11b92fb5
lanaaadev10-cmd <lanaaadev10-cmd@users.noreply.github.com>
2026-05-17 13:54:16 +0700

    feat(backend): bikin model, seeder, migrasi ProfilPerusahaan

--- app/Models/Layanan.php
+++ app/Models/Layanan.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Layanan extends Model
 {
+    use HasFactory;
+
     protected $primaryKey = 'id_layanan';
 
     protected $fillable = [
